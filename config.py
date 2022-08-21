1a75ff120ec02dce1c069ad771ccd51f3e454a24
NessaAz <vanessa.azenwa@gmail.com>
2022-08-21 23:11:11 +0300

    fix config import

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://nessa:123@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI='postgresql://nessa:123@localhost:5432/fyyur'
---
+++ migrations/versions/97507bca0001_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 97507bca0001
+Revises: 94c6784d893a
+Create Date: 2022-08-21 23:09:50.387888
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '97507bca0001'
+down_revision = '94c6784d893a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
