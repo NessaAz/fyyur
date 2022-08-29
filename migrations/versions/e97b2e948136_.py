30d6769a35872f1bfae1b6eca2e540cef94fd753
NessaAz <vanessa.azenwa@gmail.com>
2022-08-29 07:53:38 +0300

    add shows model

---
+++ migrations/versions/e97b2e948136_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e97b2e948136
+Revises: 97507bca0001
+Create Date: 2022-08-29 07:53:09.563062
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e97b2e948136'
+down_revision = '97507bca0001'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
